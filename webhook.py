c2e48057048a44b56d8a81b3bad44af0e0bc0ad4
maximus3 <max2000.gym5cheb@yandex.ru>
2020-06-10 19:39:52 +0300

    dev

--- webhook.py
+++ webhook.py
@@ -1,11 +1,12 @@
-from config import TOKEN, directory, port, host
+from TEMPLATE_config import TOKEN, directory, port, host
 
-WEBHOOK_HOST = host
-WEBHOOK_PORT = port  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+
+WEBHOOK_HOST = host  # ip/host where the bot is running
+WEBHOOK_PORT = port  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
 
 WEBHOOK_SSL_CERT = directory + 'webhook_cert.pem'  # Путь к сертификату
 WEBHOOK_SSL_PRIV = directory + 'webhook_pkey.pem'  # Путь к приватному ключу
 
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (TOKEN)
+WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(TOKEN)
